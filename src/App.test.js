551d8ad696b0f7b691c4f941b1fa4a2c915f81eb
joshuamackey <156105031+joshuamackey@users.noreply.github.com>
2024-04-02 11:51:25 -0500

    Unit test fix.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/I Want The Knife.../i);
+  const linkElement = screen.getByText(/We've got features/i);
   expect(linkElement).toBeInTheDocument();
 });
